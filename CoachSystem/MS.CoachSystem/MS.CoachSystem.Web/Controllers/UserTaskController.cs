87f4cfe6bc32577556092dab03dc3e8661ac07d6
Fahri Gedik <fahrican9978@gmail.com>
2024-12-27 20:19:43 +0300

    Create UserTask

--- CoachSystem/MS.CoachSystem/MS.CoachSystem.Web/Controllers/UserTaskController.cs
+++ CoachSystem/MS.CoachSystem/MS.CoachSystem.Web/Controllers/UserTaskController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MS.CoachSystem.Core.DTOs;
 using MS.CoachSystem.Core.DTOs.UserTaskDtos;
 using MS.CoachSystem.Core.Services;
+using System.Security.Claims;
 
 namespace MS.CoachSystem.Web.Controllers
 {
@@ -15,11 +17,34 @@ namespace MS.CoachSystem.Web.Controllers
         {
             _userTaskService = userTaskService;
         }
-        public async Task<IActionResult> Index(UserTaskRequestDto userTaskRequestDto)
+        public async Task<IActionResult> Index()
         {
-            ViewBag.ManagedStudentId = userTaskRequestDto.StudentID;
+            var coachId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var studentId = HttpContext.Session.GetString("ManagedStudentId");
+
+            var userTaskRequestDto = new UserTaskRequestDto()
+            {
+                CoachID = coachId,
+                StudentID = studentId
+            };
             var userTasks = await _userTaskService.GetUserTaskByStudentIdAsync(userTaskRequestDto);
             return View(userTasks.Data);
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(UserTaskDto userTaskDto)
+        {
+            var result = await _userTaskService.AddAsync(userTaskDto);
+            return RedirectToAction("Index", "UserTask");
         }
     }
 }
